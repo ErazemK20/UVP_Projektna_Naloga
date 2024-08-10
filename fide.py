d81c02868a885978cc94ca639fb1ec96c7ef5dd3
ErazemK20 <erazem.komelj@gmail.com>
2024-08-10 15:35:34 +0200

    izboljsana verzija

--- fide.py
+++ fide.py
@@ -12,107 +12,116 @@ import re
 import sys
 from operator import itemgetter
 
-ID1=14627500 # Zaprt interval  obravnavanih
-ID2=14629500 # identifikacijskih stevilk
-IZHODNA_DATOTEKA="seznam.txt" # Ime izhodne datoteke
+ID1 = 14625500 # Zaprt interval  obravnavanih
+ID2 = 14629500 # identifikacijskih stevilk
+IZHODNA_DATOTEKA = "seznam.txt" # Ime izhodne datoteke
 
-ROOT_URL="https://ratings.fide.com/profile/" # URL izhodiscne spletne strani
+ROOT_URL = "https://ratings.fide.com/profile/" # URL izhodiscne spletne strani
 
-NE_OBSTAJA_REGEX=r'No record found'  # Potrebni regularni...
-DRZAVA_REGEX=r'<div class="profile-top-info__block__row__header">Federation:</div>\n\s+ <div class="profile-top-info__block__row__data">(?P<drzava>[a-zA-Z\s]+)</div>'
+NE_OBSTAJA_REGEX = r'No record found'  # Potrebni regularni...
+DRZAVA_REGEX = r'<div class="profile-top-info__block__row__header">Federation:</div>\n\s+ <div class="profile-top-info__block__row__data">(?P<drzava>[a-zA-Z\s]+)</div>'
 IME_PRIIMEK_REGEX=r'<title>(?P<priimek>[a-zA-Z]+), (?P<ime>[a-zA-Z]+)</title>'
-LETO_ROJSTVA_REGEX=r'<div class="profile-top-info__block__row__header">B-Year:</div>\n\s+<div class="profile-top-info__block__row__data">(?P<leto>[\d]+)</div>'
-SPOL_REGEX=r'<div class="profile-top-info__block__row__header">Sex:</div>\n\s+<div class="profile-top-info__block__row__data">(?P<spol>[a-zA-Z]+)</div>'
-NAZIV_REGEX=r' <div class="profile-top-info__block__row__header">FIDE title:</div>\n\s+ <div class="profile-top-info__block__row__data">(?P<naziv>[a-zA-Z\s]+)</div>'
-STANDARDNI_REGEX=r'<span class="profile-top-rating-dataDesc">std</span>\n\s+(?P<standardni>\d+)'
-POSPESENI_REGEX=r'<span class="profile-top-rating-dataDesc">rapid</span>\n\s+(?P<pospeseni>\d+)'
-HITROPOTEZNI_REGEX=r'<span class="profile-top-rating-dataDesc">blitz</span>\n\s+(?P<hitropotezni>\d+)'                     # ...izrazi za iskanje v datotekah tipa html   
+LETO_ROJSTVA_REGEX = r'<div class="profile-top-info__block__row__header">B-Year:</div>\n\s+<div class="profile-top-info__block__row__data">(?P<leto>[\d]+)</div>'
+SPOL_REGEX = r'<div class="profile-top-info__block__row__header">Sex:</div>\n\s+<div class="profile-top-info__block__row__data">(?P<spol>[a-zA-Z]+)</div>'
+NAZIV_REGEX = r' <div class="profile-top-info__block__row__header">FIDE title:</div>\n\s+ <div class="profile-top-info__block__row__data">(?P<naziv>[a-zA-Z\s]+)</div>'
+STANDARDNI_REGEX = r'<span class="profile-top-rating-dataDesc">std</span>\n\s+(?P<standardni>\d+)'
+POSPESENI_REGEX = r'<span class="profile-top-rating-dataDesc">rapid</span>\n\s+(?P<pospeseni>\d+)'
+HITROPOTEZNI_REGEX = r'<span class="profile-top-rating-dataDesc">blitz</span>\n\s+(?P<hitropotezni>\d+)'                     # ...izrazi za iskanje v datotekah tipa html   
 
 
 
-seznam=[]  # Seznam (pod)seznamov
-for i in range(ID1,ID2+1):
-   url=ROOT_URL+str(i) # vsaki identifikacijski stevilki pripada locena spletna stran
-   resp=requests.get(url) # zajem ustrezne datoteke html
-   m=re.search(NE_OBSTAJA_REGEX,resp.text)
+seznam = []  # Seznam (pod)seznamov
+for i in range(ID1, ID2 + 1):
+   url = ROOT_URL + str(i) # vsaki identifikacijski stevilki pripada locena spletna stran
+   resp = requests.get(url) # zajem ustrezne datoteke html
+   m = re.search(NE_OBSTAJA_REGEX, resp.text)
    if not m: # vsak element iz [ID1,ID2] ni identifikacijska stevilka
-      print("\rZajemam podatke za FIDE ID: {}".format(i),end='')# Zajem je pocasen...
+      print("\rZajemam podatke za FIDE ID: {}".format(i), end = '')# Zajem je pocasen...
       sys.stdout.flush()
 
-      n=re.search(DRZAVA_REGEX,resp.text) # Informacija o drzavi
+      n = re.search(DRZAVA_REGEX, resp.text) # Informacija o drzavi
       if n != None:
-         drzava=n.group('drzava')
+         drzava = n.group('drzava')
       else: 
-         drzava=''
+         drzava = ''
 
-      n=re.search(IME_PRIIMEK_REGEX,resp.text) # Informacija o imenu in priimku
+      n = re.search(IME_PRIIMEK_REGEX, resp.text) # Informacija o imenu in priimku
       if n != None:
-         ime=n.group('ime')
-         priimek=n.group('priimek')
+         ime = n.group('ime')
+         priimek = n.group('priimek')
       else: 
-         ime=''
-         priimek=''
+         ime = ''
+         priimek = ''
 
-      n=re.search(LETO_ROJSTVA_REGEX,resp.text) # Informacija o letnici rojstva
+      n = re.search(LETO_ROJSTVA_REGEX, resp.text) # Informacija o letnici rojstva
       if n != None:
-         leto=n.group('leto') 
-      else: leto=''
+         leto = n.group('leto') 
+      else: leto = ''
 
-      n=re.search(SPOL_REGEX,resp.text) # Informacija o spolu 
+      n = re.search(SPOL_REGEX, resp.text) # Informacija o spolu 
       if n != None:
          spol = "Moski" if n.group('spol') == "Male" else "Zenska"
-      else: spol=''
+      else: spol = ''
 
-      n=re.search(NAZIV_REGEX,resp.text) # Informacija o nazivu FIDE
+      n = re.search(NAZIV_REGEX, resp.text) # Informacija o nazivu FIDE
       if n != None:
          if n.group('naziv') == "FIDE Master":
-            naziv="Mojster FIDE"
+            naziv = "FIDE Mojster"
          elif n.group('naziv') == "International Master":
-            naziv="Mednarodni mojster"
+            naziv = "Mednarodni mojster"
          elif n.group('naziv') == "Grandmaster":
-            naziv="Velemojster"
+            naziv = "Velemojster"
          elif n.group('naziv') == "Woman FIDE Master":
-            naziv="Mojstrica FIDE"
+            naziv = "FIDE Mojstrica"
          elif n.group('naziv') == "Woman International Master":
-            naziv="Mednarodna mojstrica"
+            naziv = "Mednarodna mojstrica"
          elif n.group('naziv') == "Woman Grandmaster":
-            naziv="Velemojstrica"
+            naziv = "Velemojstrica"
          else:
-            naziv=''
-      else: naziv='' 
+            naziv = ''
+      else: naziv = '' 
 
-      n=re.search(STANDARDNI_REGEX,resp.text) # Informacija o standardnem rangu
+      n = re.search(STANDARDNI_REGEX, resp.text) # Informacija o standardnem rangu
       if n != None:
-         standardni=n.group('standardni')
+         standardni = n.group('standardni')
       else: standardni = '' 
-      n=re.search(POSPESENI_REGEX,resp.text) # Informacija o pospesenem rangu
+      n = re.search(POSPESENI_REGEX, resp.text) # Informacija o pospesenem rangu
       if n != None:
-         pospeseni=n.group('pospeseni')
+         pospeseni = n.group('pospeseni')
       else: pospeseni = '' 
 
-      n=re.search(HITROPOTEZNI_REGEX,resp.text) # Informacija o hitropoteznem rangu
+      n = re.search(HITROPOTEZNI_REGEX,resp.text) # Informacija o hitropoteznem rangu
       if n != None:
-         hitropotezni=n.group('hitropotezni')
+         hitropotezni = n.group('hitropotezni')
       else: hitropotezni = '' 
      
-      sahist=[i,drzava,ime,priimek,leto,spol,naziv,standardni,pospeseni,hitropotezni]
-      if(drzava=="Slovenia" and standardni!=""): # Podseznam za slovenskega sahista
+      sahist=[i, drzava, ime, priimek, leto, spol, naziv, standardni, pospeseni, hitropotezni]
+      if(drzava == "Slovenia" and standardni != "" and pospeseni != "" and hitropotezni != ""): # Podseznam za slovenskega sahista
          seznam.append(sahist)                   # uvrstimo v glavni seznam
 
-urejen_seznam=sorted(seznam,key=itemgetter(7),reverse=True) # Seznam uredimo glede na
+urejen_seznam = sorted(seznam, key = itemgetter(7), reverse = True) # Seznam uredimo glede na
 print ()                                                    # standardni rang
 
-standard_povprecje=0   # Za dolocanje povprecenga standardnega ranga
+standard_povprecje = 0   # Za dolocanje povprecnega standardnega ranga
+pospeseni_povprecje = 0   # Za dolocanje povprecnega pospesenega ranga
+hitropotezni_povprecje = 0    # Za dolocanje povprecnega hitropoteznega ranga
 
-f=open(IZHODNA_DATOTEKA,'w') # Odpri izhodno datoteko za izpis
+f = open(IZHODNA_DATOTEKA, 'w') # Odpri izhodno datoteko za izpis
 for sahist in urejen_seznam:
-   print (sahist[7],sahist[2],sahist[3],",","roj.",sahist[4],",",sahist[6])
-   standard_povprecje+=int(sahist[7])
-   vrstica=str(sahist[7])+" "+str(sahist[2])+" "+str(sahist[3])+" "+str(sahist[4])+" "+str(sahist[6]+"\n")
+   print (sahist[7], sahist[8], sahist[9], sahist[2], sahist[3], ",", "roj.", sahist[4], ",", sahist[6])
+   standard_povprecje += int(sahist[7])
+   pospeseni_povprecje += int(sahist[8])
+   hitropotezni_povprecje += int(sahist[9])
+   vrstica = str(sahist[7]) + " " + str(sahist[8]) + " " + str(sahist[9]) + " " + str(sahist[2]) + " " + str(sahist[3]) + " " + str(sahist[4]) + " " + str(sahist[6] + "\n")
    f.write(vrstica)
 
 f.close()
 
-standard_povprecje/=len(urejen_seznam)
-print ("Povprecen standardni rang: ",int(standard_povprecje))
+standard_povprecje /= len(urejen_seznam)
+pospeseni_povprecje /= len(urejen_seznam)
+hitropotezni_povprecje /= len(urejen_seznam)
+
+print ("Povprecen standardni rang: ", int(standard_povprecje))
+print ("Povprecen pospeseni rang: ", int(pospeseni_povprecje))
+print ("Povprecen hitropotezni rang: ", int(hitropotezni_povprecje))
       
--- fide_graf.py
+++ fide_graf.py
@@ -7,41 +7,69 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-N=10 #Stevilo stolpcev
+N = 10 #Stevilo stolpcev
 
-seznam=[]
-letnice=[]
+seznam = []
+seznamp = []
+seznamh = []
+letnice = []
 def graf(datoteka):
    try:                                   #Odpiranje datoteke, ustvarjene s fide.py
-      f=open(datoteka,'r')
+      f = open(datoteka, 'r')
    except FileNotFoundError: 
       print("Datoteka {}  ne obstaja".format(datoteka))
       return None
        
-   podatki=f.readlines()                  #Branje iz datoteke
+   podatki = f.readlines()                  #Branje iz datoteke
    for vrstica in podatki:
-      m=re.search("(?P<rang>\d+)[a-zA-Z\s]+(?P<leto>\d+)",vrstica)
-      if m != None:                       #Podatki vsebujejo rang in letnico rojstva
-         rang=m.group('rang')
-         leto=m.group('leto')
+      m = re.search("(?P<rang>\d+)\s+(?P<rangp>\d+)\s+(?P<rangh>\d+)\s+[a-zA-Z\s]+(?P<leto>\d+)", vrstica)
+      if m != None:                       #Podatki vsebujejo rang, pospeseni rang, hitropotezni rang in letnico rojstva
+         rang = m.group('rang')
+         rangp = m.group('rangp')
+         rangh = m.group('rangh')
+         leto = m.group('leto')
          seznam.append(int(rang))
+         seznamp.append(int(rangp))
+         seznamh.append(int(rangh))
          letnice.append(int(leto))
    f.close()                              #Zapiranje datoteke
 
-   urejen_seznam=sorted(seznam)           #Narascajoci rangi
+   urejen_seznam = sorted(seznam)           #Narascajoci rangi
+   urejen_seznamp = sorted(seznamp)
+   urejen_seznamh = sorted(seznamh)
 
-   sp=min(urejen_seznam)
-   zg=max(urejen_seznam)
+   sp = min(urejen_seznam)
+   zg = max(urejen_seznam)
 
-   plt.title("Porazdelitev standardnih rangov")                       #Risanje histograma
+   spp = min(urejen_seznamp)
+   zgp = max(urejen_seznamp)
+
+   sph = min(urejen_seznamh)
+   zgh = max(urejen_seznamh)
+
+   plt.title("Porazdelitev standardnih rangov")                       #Risanje histogramov
    plt.xlabel("Rang")
    plt.ylabel("Stevilo igralcev")
-   plt.hist(urejen_seznam,bins=N,color='skyblue',edgecolor='black')
-   plt.xticks(np.arange(sp,zg+1,int((zg-sp)/N)))
+   plt.hist(urejen_seznam, bins = N, color = 'skyblue', edgecolor = 'black')
+   plt.xticks(np.arange(sp, zg + 1, int((zg - sp) / N)))
+   plt.show()
+
+   plt.title("Porazdelitev pospesenih rangov")
+   plt.xlabel("Rang")
+   plt.ylabel("Stevilo igralcev")
+   plt.hist(urejen_seznam, bins = N, color = 'skyblue', edgecolor = 'black')
+   plt.xticks(np.arange(spp, zgp + 1, int((zgp - spp) / N)))
+   plt.show()
+
+   plt.title("Porazdelitev hitropoteznih rangov")
+   plt.xlabel("Rang")
+   plt.ylabel("Stevilo igralcev")
+   plt.hist(urejen_seznam, bins = N, color = 'skyblue', edgecolor = 'black')
+   plt.xticks(np.arange(sph, zgh + 1, int((zgh - sph) / N)))
    plt.show()
 
    plt.title("Korelacija med standardnim rangom in letnico rojstva")  #Risanje raztresenega diagrama
    plt.xlabel("Letnica rojstva")
    plt.ylabel("Rang")
-   plt.scatter(letnice,seznam)
+   plt.scatter(letnice, seznam)
    plt.show()
